07f3edf49aac79e7fb93cd323c7a279744df8c95
Cam <vanicellic@yahoo.com>
2019-10-21 02:08:27 -0500

    throws error if fields are left blank

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 // Write your JavaScript code here!
+window.addEventListener("load", function() {
+   let form = document.getElementById("formID");
+   let pilotName= document.getElementById("pilotName")
+   let copilotName= document.getElementById("copilotName")
+   let cargoMass= document.getElementById("cargoMass")
+   let fuelLevel= document.getElementById("fuelLevel")
+   form.addEventListener("submit", function(event){
+      if (pilotName.value === '' || copilotName.value === '' || cargoMass.value === "" || fuelLevel.value === ""){
+         alert("All Fields Required!")
+         event.preventDefault();
+      }
+
+      if (typeof(pilotName.value) === "number"){
+         alert("not a string")
+      }
+   })
+});
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
