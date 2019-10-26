76d608c2a66029976c5c05615ed5efa06f69c752
Cam <vanicellic@yahoo.com>
2019-10-26 12:22:13 -0500

    Correct fuel and cargo return messages

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ window.addEventListener("load", function() {
    let fuelLevel= document.getElementById("fuelLevel")
    let faultyItems = document.querySelector('#faultyItems')
    let fuelStatus= document.getElementById("fuelStatus")
+   let cargoStatus= document.getElementById("cargoStatus")
 
    form.addEventListener("submit", function(event){
       if (pilotName.value === '' || copilotName.value === '' || cargoMass.value === "" || fuelLevel.value === ""){
@@ -20,10 +21,11 @@ window.addEventListener("load", function() {
 
       if(cargoMass.value > 10000){
          faultyItems.style.visibility = "visible";
+         cargoStatus.innerHTML = "You are Overweight.  Cargo is too Heavy"
          event.preventDefault();
       } else if(fuelLevel.value < 10000){
          faultyItems.style.visibility = "visible"
-         // element.setAttribute(fuelStatus, "Fuel Level too Low")
+         fuelStatus.innerHTML = "Fuel Level too Low"
          event.preventDefault();
       }
    })
