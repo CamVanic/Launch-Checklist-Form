7bba7468819aaf0a28fe93904ee15c255a387219
Cam <vanicellic@yahoo.com>
2019-10-26 13:40:23 -0500

    finished part 2

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ window.addEventListener("load", function() {
    let cargoMass= document.getElementById("cargoMass")
    let fuelLevel= document.getElementById("fuelLevel")
    let faultyItems = document.querySelector('#faultyItems')
+   let launchStatus = document.getElementById("launchStatus")
    let fuelStatus= document.getElementById("fuelStatus")
    let cargoStatus= document.getElementById("cargoStatus")
 
@@ -13,18 +14,20 @@ window.addEventListener("load", function() {
       if (pilotName.value === '' || copilotName.value === '' || cargoMass.value === "" || fuelLevel.value === ""){
          alert("All Fields Required!")
          event.preventDefault();
-      }
-
-      if(!isNaN(pilotName.value) || !isNaN(copilotName.value)){
+      } else if(!isNaN(pilotName.value) || !isNaN(copilotName.value)){
          alert("Invalid Input")
       }
 
       if(cargoMass.value > 10000){
          faultyItems.style.visibility = "visible";
+         launchStatus.style.color = "red"
+         launchStatus.innerHTML = "Shuttle Not Ready for Launch"
          cargoStatus.innerHTML = "You are Overweight.  Cargo is too Heavy"
          event.preventDefault();
       } else if(fuelLevel.value < 10000){
          faultyItems.style.visibility = "visible"
+         launchStatus.style.color = "red"
+         launchStatus.innerHTML = "Shuttle Not Ready for Launch"
          fuelStatus.innerHTML = "Fuel Level too Low"
          event.preventDefault();
       }
