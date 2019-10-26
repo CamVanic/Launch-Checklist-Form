92700c91dac63356e41e0ab5797d0cd77ceb4a69
Cam <vanicellic@yahoo.com>
2019-10-26 11:34:16 -0500

    Trying to make conditionals for fuelStatus and cargoMass

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ window.addEventListener("load", function() {
    let copilotName= document.getElementById("copilotName")
    let cargoMass= document.getElementById("cargoMass")
    let fuelLevel= document.getElementById("fuelLevel")
+   let faultyItems = document.querySelector('#faultyItems')
+   let fuelStatus= document.getElementById("fuelStatus")
+
    form.addEventListener("submit", function(event){
       if (pilotName.value === '' || copilotName.value === '' || cargoMass.value === "" || fuelLevel.value === ""){
          alert("All Fields Required!")
@@ -14,8 +17,19 @@ window.addEventListener("load", function() {
       if(!isNaN(pilotName.value) || !isNaN(copilotName.value)){
          alert("Invalid Input")
       }
+
+      if(cargoMass.value > 10000){
+         faultyItems.style.visibility = "visible";
+         event.preventDefault();
+      } else if(fuelLevel.value < 10000){
+         faultyItems.style.visibility = "visible"
+         // element.setAttribute(fuelStatus, "Fuel Level too Low")
+         event.preventDefault();
+      }
    })
 });
+
+
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
